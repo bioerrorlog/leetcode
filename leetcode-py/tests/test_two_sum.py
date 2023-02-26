ca9808f46c0d5851805a61d5628d12dae012128a
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-02-26 18:26:41 +0900

    update: parametrized test

--- leetcode-py/tests/test_two_sum.py
+++ leetcode-py/tests/test_two_sum.py
@@ -1,13 +1,12 @@
+import pytest
 from leetcode_py.two_sum import Solution
 
 
-def test_two_sum():
+@pytest.mark.parametrize('nums, target, expected',
+    [
+        ([2, 7, 11, 15], 9, [0, 1]),
+    ]
+)
+def test_two_sum(nums, target, expected):
     sol = Solution()
-
-    # Input
-    nums = [2, 7, 11, 15]
-    target = 9
-
-    expected = [0, 1]
-
     assert sol.two_sum(nums, target) == expected
