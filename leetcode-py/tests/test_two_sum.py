07ff5841269c4350878556efbd51affb2ac7206d
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-02-26 18:31:59 +0900

    add test cases

--- leetcode-py/tests/test_two_sum.py
+++ leetcode-py/tests/test_two_sum.py
@@ -5,6 +5,8 @@ from leetcode_py.two_sum import Solution
 @pytest.mark.parametrize('nums, target, expected',
     [
         ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 0, [6, 1]),
     ]
 )
 def test_two_sum(nums, target, expected):
