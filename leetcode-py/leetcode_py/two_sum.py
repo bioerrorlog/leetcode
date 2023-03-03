24cbaf34674a8425b9f58255cc8e3c7e833165e8
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-03-03 13:12:16 +0900

    move comments

--- leetcode-py/leetcode_py/two_sum.py
+++ leetcode-py/leetcode_py/two_sum.py
@@ -1,8 +1,8 @@
-# Problem 1.
-# https://leetcode.com/problems/two-sum/
 from typing import List
 
 
+# Problem 1.
+# https://leetcode.com/problems/two-sum/
 class Solution:
     def two_sum(self, nums: List[int], target: int) -> List[int]:
         prev_vals = {}  # val: index
