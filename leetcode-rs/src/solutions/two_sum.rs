8750a61219f9957bf0c60061185273bc03c9eced
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-03-06 21:54:52 +0900

    add test for two_sum

--- leetcode-rs/src/solutions/two_sum.rs
+++ leetcode-rs/src/solutions/two_sum.rs
@@ -21,3 +21,18 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     // Because each input would have exactly one solution.
     unreachable!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![2, 7, 11, 15], 9, vec![0, 1])]
+    #[case(vec![3, 2, 4], 6, vec![1, 2])]
+    #[case(vec![3, 3], 6, vec![0, 1])]
+    fn two_sum_test(#[case] nums: Vec<i32>, #[case] target: i32, #[case] expected: Vec<i32>) {
+        let result = two_sum(nums, target);
+        assert_eq!(result, expected);
+    }
+}
